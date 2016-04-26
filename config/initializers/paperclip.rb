2bdca048e8b8be0cdf694f923f19f6a21b3623f6
VoroninNick <p.korenev@voroninstudio.eu>
2016-04-26 13:57:29 +0300

    app

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
-Paperclip::Attachment.default_options[:url] = "/:class/:id/:style/:basename.:extension"
+Paperclip::Attachment.default_options[:url] = "/:class/:id/:attachment/:style/:basename.:extension"
 Paperclip::Attachment.default_options[:path] = "public/:url"
