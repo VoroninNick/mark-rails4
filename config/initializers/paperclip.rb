3b0a4d2fb4a1c3ee44015e1d3b331903335c01b4
VoroninNick <p.korenev@voroninstudio.eu>
2016-04-26 14:45:27 +0300

    app

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -6,12 +6,17 @@ Paperclip::Attachment.default_options[:old_path] = "/system/:url"
 module PaperclipExtension
   def move_images(old_path_pattern = Paperclip::Attachment.default_options[:old_path], new_path_pattern = Paperclip::Attachment.default_options[:path])
     if self.try(:attachment_definitions).present?
+      puts "model: #{self.name}"
       self.all.each do |instance|
+        puts "..id: #{instance.id}"
         self.attachment_definitions.each do |attachment_name, attachment_definition|
+          puts "....attachment_name: #{attachment_name}"
           attachment = instance.send(attachment_name)
           style_names = (attachment.styles.keys.map(&:to_s) + ["original"]).uniq
           style_names.each do |style_name|
+            puts "......style_name: #{style_name}"
             old_path = attachment.send(:interpolate, old_path_pattern, style_name)
+            puts "......old_path: #{old_path}; exists: #{File.exists?(old_path).inspect}"
             if File.exists?(old_path)
               new_path = attachment.send(:interpolate, new_path_pattern, style_name)
               new_dirname = File.dirname(new_path)
